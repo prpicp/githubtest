28ad08250a7e9968d942e756c80ccba006aaff8f
Paris <prpicp@macstudent.com.au>
2020-06-07 17:46:00 +1000

    first

---
+++ meanings.py
@@ -0,0 +1,12 @@
+import
+print("1 - the magic computers")
+print("2 - band camp")
+print("3 - graduation")
+print("4 - farm animals")
+print("5 - birthday surprise")
+print("6 - a hike in the mountains")
+choice = input("which mad libs would you like to do?")
+text = libs[choice]
+
+
+#if "help" is written then show tool tip
